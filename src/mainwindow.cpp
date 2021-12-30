404a205a5ea759cd4af42941fe142c741fde2bc7
Glitched Polygons <glitchedpolygons@protonmail.com>
2021-12-30 12:17:59 +0100

    Alright, it works :D  now needs the verification funcitonality too + some polishing :>

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,10 +1,6 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 
-extern "C" {
-#include <argon2.h>
-}
-
 #ifdef _WIN32
 #define WIN32_NO_STATUS
 #include <windows.h>
@@ -12,6 +8,7 @@ extern "C" {
 #include <bcrypt.h>
 #endif
 
+#include <argon2.h>
 #include <QDateTime>
 #include <QCryptographicHash>
 
@@ -36,15 +33,35 @@ static inline void dev_urandom(uint8_t* outputBuffer, const size_t outputBufferS
     }
 }
 
+static int(*hashFunction)(const uint32_t, const uint32_t,
+                          const uint32_t, const void*,
+                          const size_t, const void*,
+                          const size_t, const size_t,
+                          char*, const size_t) = &argon2id_hash_encoded;
+
+static const char* plural[] = {
+    "",
+    "s"
+};
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
     uint8_t initialEntropy [32];
     dev_urandom(initialEntropy,sizeof(initialEntropy));
     userEntropy = QString(reinterpret_cast<char*>(initialEntropy));
     appendEntropy(QDateTime::currentDateTime().toString(Qt::DateFormat::RFC2822Date));
+
+    ui->hashAlgorithmButtonGroup->setId(ui->argon2idRadioButton, 0);
+    ui->hashAlgorithmButtonGroup->setId(ui->argon2iRadioButton, 1);
+    ui->hashAlgorithmButtonGroup->setId(ui->argon2dRadioButton, 2);
+
+    QObject::connect(ui->hashAlgorithmButtonGroup, SIGNAL(idClicked(int)), this, SLOT(onChangedHashAlgorithm(int)));
+
+    ui->passwordLineEdit->setFocus();
 }
 
 MainWindow::~MainWindow()
@@ -54,8 +71,11 @@ MainWindow::~MainWindow()
 
 void MainWindow::appendEntropy(const QString& additionalEntropy)
 {
+    const QString timestamp = QDateTime::currentDateTime().toString(Qt::DateFormat::ISODateWithMs);
+
     QCryptographicHash sha256(QCryptographicHash::Algorithm::Sha256);
-    sha256.addData((userEntropy + additionalEntropy).toUtf8());
+    sha256.addData((userEntropy + additionalEntropy + timestamp).toUtf8());
+
     userEntropy = QString(sha256.result());
 }
 
@@ -68,13 +88,14 @@ void MainWindow::on_clearButton_clicked()
 
 void MainWindow::on_hashButton_clicked()
 {
-    if (busy){
+    if (busy)
+    {
         return;
     }
 
     busy = true;
     ui->encodedHashTextEdit->setText("Working on it... CPU go brr!");
-    ui->hashButtonsHorizontalLayout->setEnabled(false);
+    repaint();
 
     uint8_t salt[32] = {0x00};
     char encodedHash[1024] = {0x00};
@@ -87,24 +108,102 @@ void MainWindow::on_hashButton_clicked()
 
     const QByteArray passwordUtf8Bytes = password.toUtf8();
     const char* passwordUtf8 =  passwordUtf8Bytes.constData();
-    const size_t passwordUtf8Length = strlen(passwordUtf8); // This is OK because according to Qt documentation, .constData() returns a NUL-terminated buffer.
+    const size_t passwordUtf8Length = strlen(passwordUtf8); // This is OK and perfectly safe because, according to Qt documentation, ".constData()" returns a NUL-terminated buffer.
 
     const int argon2_timeCost = ui->timeCostHorizontalSlider->value();
     const int argon2_memoryCostMiB = ui->memoryCostHorizontalSlider->value();
     const int argon2_parallelism = ui->parallelismHorizontalSlider->value();
     const int hashLength = ui->hashLengthHorizontalSlider->value();
 
-    int r = argon2id_hash_encoded(static_cast<uint32_t>(argon2_timeCost), static_cast<uint32_t>(argon2_memoryCostMiB) * 1024, argon2_parallelism, passwordUtf8, passwordUtf8Length, salt, sizeof(salt), static_cast<size_t>(hashLength), encodedHash, sizeof(encodedHash));
-    if (r != ARGON2_OK)
+    const int r = hashFunction
+    (
+        static_cast<uint32_t>(argon2_timeCost),
+        static_cast<uint32_t>(argon2_memoryCostMiB) * 1024,
+        argon2_parallelism,
+        passwordUtf8,
+        passwordUtf8Length,
+        salt,
+        sizeof(salt),
+        static_cast<size_t>(hashLength),
+        encodedHash,
+        sizeof(encodedHash)
+    );
+
+    if (r == ARGON2_OK)
     {
-        fprintf(stderr, "Argon2UI: argon2id failure! \"argon2id_hash_raw\" returned: %d\n", r);
-        goto exit;
+        output = QString(encodedHash);
+    }
+    else
+    {
+        char error[1024] = {0x00};
+        snprintf(error, sizeof(error), "Argon2 hash generation failed! \"%s\" function call returned: %d\n", getHashFunctionName(), r);
+        fprintf(stderr, "%s", error);
+        output = QString(error);
     }
 
-    output = QString(encodedHash);
-    ui->encodedHashTextEdit->setText(output);
-
-    exit:
     busy = false;
+    ui->encodedHashTextEdit->setText(output);
+}
+
+const char* MainWindow::getHashFunctionName() const
+{
+    switch(ui->hashAlgorithmButtonGroup->checkedId())
+    {
+        default:
+            return "argon2id_hash_encoded";
+        case 1:
+            return "argon2i_hash_encoded";
+        case 2:
+            return "argon2d_hash_encoded";
+    }
+}
+
+void MainWindow::onChangedHashAlgorithm(int id)
+{
+    switch(id)
+    {
+        default:
+            hashFunction = &argon2id_hash_encoded;
+            break;
+        case 1:
+            hashFunction = &argon2i_hash_encoded;
+            break;
+        case 2:
+            hashFunction = &argon2d_hash_encoded;
+            break;
+    }
+}
+
+void MainWindow::on_timeCostHorizontalSlider_valueChanged(int value)
+{
+    QString newLabelText = QString("Time cost (%1 iteration%2)").arg(value).arg(plural[value > 1]);
+    ui->timeCostLabel->setText(newLabelText);
+    appendEntropy(newLabelText);
+}
+
+void MainWindow::on_memoryCostHorizontalSlider_valueChanged(int value)
+{
+    QString newLabelText = QString("Memory cost (%1 MiB)").arg(value);
+    ui->memoryCostLabel->setText(newLabelText);
+    appendEntropy(newLabelText);
+}
+
+void MainWindow::on_parallelismHorizontalSlider_valueChanged(int value)
+{
+    QString newLabelText = QString("Parallelism (%1 thread%2)").arg(value).arg(plural[value > 1]);
+    ui->parallelismLabel->setText(newLabelText);
+    appendEntropy(newLabelText);
+}
+
+void MainWindow::on_hashLengthHorizontalSlider_valueChanged(int value)
+{
+    QString newLabelText = QString("Hash length (%1 B)").arg(value);
+    ui->hashLengthLabel->setText(newLabelText);
+    appendEntropy(newLabelText);
+}
+
+void MainWindow::on_encodedHashTextEdit_selectionChanged()
+{
+    appendEntropy(ui->encodedHashTextEdit->textCursor().selectedText());
 }
 
--- src/mainwindow.h
+++ src/mainwindow.h
@@ -20,11 +20,24 @@ private slots:
 
     void on_hashButton_clicked();
 
+    void on_timeCostHorizontalSlider_valueChanged(int value);
+
+    void on_memoryCostHorizontalSlider_valueChanged(int value);
+
+    void on_parallelismHorizontalSlider_valueChanged(int value);
+
+    void on_hashLengthHorizontalSlider_valueChanged(int value);
+
+    void onChangedHashAlgorithm(int id);
+
+    void on_encodedHashTextEdit_selectionChanged();
+
 private:
     Ui::MainWindow *ui;
     bool busy = false;
     QString userEntropy;
 
     void appendEntropy(const QString& entropy);
+    const char* getHashFunctionName() const;
 };
 #endif // MAINWINDOW_H
